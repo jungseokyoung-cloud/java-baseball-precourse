19255e0315c96a288997ccf1f1c2a1f21bc879b9
rui.bao <thrdud0423@gmail.com>
2026-01-28 17:33:20 +0900

    feat(domain): BaseballNumbers 도메인 클래스 추가

---
+++ src/main/java/Domains/BaseballNumbers.java
@@ -0,0 +1,12 @@
+package Domains;
+
+import java.util.List;
+import java.util.Objects;
+
+public class BaseballNumbers {
+    private final List<Integer> numbers;
+
+    public BaseballNumbers(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+}
