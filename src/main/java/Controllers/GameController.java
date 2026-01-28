c8f15505b8286e0079adef4ae16db2036ebebc4c
rui.bao <thrdud0423@gmail.com>
2026-01-28 17:12:02 +0900

    [FEAT]: GameController 추가

---
+++ src/main/java/Controllers/GameController.java
@@ -0,0 +1,13 @@
+package Controllers;
+
+import Views.InputView;
+
+public class GameController {
+    private final InputView inputView;
+
+    public GameController(InputView inputView) {
+        this.inputView = inputView;
+    }
+
+    public void run() { }
+}
