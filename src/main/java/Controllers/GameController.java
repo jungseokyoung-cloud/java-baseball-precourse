68af7469c35f5a750a1a36b6ebc5e8b1f1bc25b2
rui.bao <thrdud0423@gmail.com>
2026-01-28 18:17:27 +0900

    feat(controller): 게임 실행 루프 및 재시작/종료 로직 구현
    - run() 메서드를 통한 전체 게임 흐름 제어 추가
    - 사용자의 재시작(1) 및 종료(2) 선택 기능 구현
    - 잘못된 입력 시 에러 메시지 출력 후 재입력을 받는 예외 처리 로직 추가

--- src/main/java/Controllers/GameController.java
+++ src/main/java/Controllers/GameController.java
@@ -1,5 +1,6 @@
 package Controllers;
 
+import Domains.Validator;
 import Views.InputView;
 
 public class GameController {
@@ -9,5 +10,22 @@ public class GameController {
         this.inputView = inputView;
     }
 
-    public void run() { }
+    public void run() {
+        do {
+            playGame();
+        } while (isRestartRequested());
+    }
+
+    private void playGame() { }
+
+    private boolean isRestartRequested() {
+        try {
+            String command = inputView.readRestartCommand();
+            Validator.validateRestartCommand(command);
+            return command.equals("1");
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+            return isRestartRequested(); // 잘못 입력하면 재귀적으로 다시 물어봄
+        }
+    }
 }
