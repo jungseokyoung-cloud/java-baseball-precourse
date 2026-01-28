d79fc594fdfc5a3f6e9e523726159fa740ce3339
rui.bao <thrdud0423@gmail.com>
2026-01-28 17:34:55 +0900

    feat(domain): 중복 없는 숫자 생성기(NumberGenerator) 구현
    1~9 사이의 서로 다른 임의의 숫자 3개를 생성하여 BaseballNumbers를 반환하는 로직 추가

---
+++ src/main/java/Domains/NumberGenerator.java
@@ -0,0 +1,24 @@
+package Domains;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class NumberGenerator {
+    private static final int NUMBER_COUNT = 3;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+
+    public static BaseballNumbers generate() {
+        Set<Integer> uniqueNumbers = new LinkedHashSet<>();
+        Random random = new Random();
+
+        while (uniqueNumbers.size() < NUMBER_COUNT) {
+            int randomNumber = random.nextInt(MAX_NUMBER - MIN_NUMBER + 1) + MIN_NUMBER;
+            uniqueNumbers.add(randomNumber);
+        }
+
+        return new BaseballNumbers(new ArrayList<>(uniqueNumbers));
+    }
+}
